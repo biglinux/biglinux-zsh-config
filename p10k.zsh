b920c8561dd504b5e0b8866f30cb6733b1f40452
talesam <talesam@gmail.com>
2023-02-22 23:10:17 -0300

    Swaping icon term

--- p10k.zsh
+++ p10k.zsh
@@ -188,7 +188,7 @@
   typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=232
   typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=7
   # Custom icon.
-  typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='👉'
+  typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='👽'
 
   ################################[ prompt_char: prompt symbol ]################################
   # Transparent background.
