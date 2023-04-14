3eea81c0504ac5570c2345a5dcfd17623b273e8a
talesam <talesam@gmail.com>
2023-04-13 23:49:00 -0300

    Add icon Biglinux

--- p10k.zsh
+++ p10k.zsh
@@ -188,7 +188,7 @@
   typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=232
   typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=7
   # Custom icon.
-  typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='👽'
+  typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION=' ' 
 
   ################################[ prompt_char: prompt symbol ]################################
   # Transparent background.
