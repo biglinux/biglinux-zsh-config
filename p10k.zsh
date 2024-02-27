e7d4b36cc912361f05e86c264b32277fca3f4678
talesam <talesam@gmail.com>
2024-02-27 13:01:45 -0300

    Changed icon biglinux

--- p10k.zsh
+++ p10k.zsh
@@ -188,7 +188,7 @@
   typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=254
   typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=24
   # Custom icon Biglinux
-  typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION=' ' 
+  typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION=' ' 
 
   ################################[ prompt_char: prompt symbol ]################################
   # Transparent background.
